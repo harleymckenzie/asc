bf3a3f7afad577331524eec6ad8fc1f758797ba6
Harley McKenzie <mckenzie.harley@gmail.com>
2024-02-27 11:17:06 +0000

    Update application version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='asc',
-    version='0.1.0',
+    version='0.0.1',
     packages=find_packages(),
     include_package_data=True,
     scripts=['asc.py'],
