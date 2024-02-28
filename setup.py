a4ac6728bc09c2e208930cefe8bc9143c1b803b4
Harley McKenzie <mckenzie.harley@gmail.com>
2024-02-28 13:35:27 +0000

    Update structure to better support installation

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='asc',
     version='0.0.1',
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests*']),
     include_package_data=True,
-    scripts=['asc.py'],
+    scripts=['cli/asc'],
     install_requires=[
         'argparse',
         'boto3',
