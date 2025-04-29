162b75cc8a1453ee5b97645c8a8add3184a91027
Harley McKenzie <mckenzie.harley@gmail.com>
2025-04-29 14:10:29 +0100

    Add support for relative time to asg schedule

--- pkg/shared/timeformat/timeformat.go
+++ pkg/shared/timeformat/timeformat.go
@@ -1,6 +1,8 @@
 package timeformat
 
 import (
+	"fmt"
+	"strings"
 	"time"
 
 	"github.com/olebedev/when"
@@ -8,7 +10,35 @@ import (
 	"github.com/olebedev/when/rules/common"
 )
 
+var durationMap = map[string]string{
+    "hour":   "h",
+    "hours":  "h",
+    "minute": "m",
+    "minutes": "m",
+    "day":    "h24",
+    "days":   "h24",
+    "week":   "h168",
+    "weeks":  "h168",
+}
+
 func ParseTime(timeStr string) (time.Time, error) {
+	
+    // Try parsing as human-readable duration (e.g., "2 hours")
+    fields := strings.Fields(timeStr)
+    if len(fields) == 2 {
+        if unit, ok := durationMap[strings.ToLower(fields[1])]; ok {
+            durStr := fields[0] + unit
+            if duration, err := time.ParseDuration(durStr); err == nil {
+                return time.Now().Add(duration), nil
+            }
+        }
+    }
+
+	// Fall back to natural language parsing
+	if parsed, err := time.Parse(time.RFC3339, timeStr); err == nil {
+		return parsed, nil
+	}
+
 	w := when.New(nil)
 	w.Add(en.All...)
 	w.Add(common.All...)
@@ -16,6 +46,10 @@ func ParseTime(timeStr string) (time.Time, error) {
 	parsed, err := w.Parse(timeStr, time.Now())
 	if err != nil {
 		return time.Time{}, err
+	}
+
+	if parsed == nil {
+		return time.Time{}, fmt.Errorf("invalid: %s", timeStr)
 	}
 
 	return parsed.Time, nil
