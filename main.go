ba80494be6b828ea294ef93a7346481e8a049aae
Harley McKenzie <mckenzie.harley@gmail.com>
2025-05-07 15:03:29 +0100

    Remove references to services in main.go, which are no longer required

--- main.go
+++ main.go
@@ -2,10 +2,6 @@ package main
 
 import (
 	"github.com/harleymckenzie/asc/cmd"
-	_ "github.com/harleymckenzie/asc/cmd/cloudformation"
-	_ "github.com/harleymckenzie/asc/cmd/ec2"
-	_ "github.com/harleymckenzie/asc/cmd/elasticache"
-	_ "github.com/harleymckenzie/asc/cmd/rds"
 )
 
 func main() {
