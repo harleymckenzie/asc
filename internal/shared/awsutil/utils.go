3442a817a0ff4d1e3c2c032ccd22de78820d48d2
Harley McKenzie <mckenzie.harley@gmail.com>
2025-09-24 14:12:00 +0100

    Remove old tablewriter package

--- internal/shared/awsutil/utils.go
+++ internal/shared/awsutil/utils.go
@@ -5,19 +5,23 @@ import (
 	"reflect"
 
 	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
-	"github.com/harleymckenzie/asc/internal/shared/tableformat"
 	"github.com/harleymckenzie/asc/internal/shared/tablewriter"
 )
 
+type Tag struct {
+	Name  string
+	Value string
+}
+
 // NormalizeTags turns the provided tags into a map
-func NormalizeTags(tags any) ([]tableformat.Tag, error) {
-	var result []tableformat.Tag
+func NormalizeTags(tags any) ([]Tag, error) {
+	var result []Tag
 
 	// EC2
 	if t, ok := tags.([]types.Tag); ok {
 		for _, tag := range t {
 			if tag.Key != nil && tag.Value != nil {
-				result = append(result, tableformat.Tag{
+				result = append(result, Tag{
 					Name:  *tag.Key,
 					Value: *tag.Value,
 				})
