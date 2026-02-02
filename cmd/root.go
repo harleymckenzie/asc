9c62fc902d116969b80cd12a8e19101635305dd8
Harley McKenzie <mckenzie.harley@gmail.com>
2026-02-02 22:26:48 +0000

    Version bump

--- cmd/root.go
+++ cmd/root.go
@@ -17,7 +17,7 @@ import (
 var (
 	Profile string     // AWS profile to use for authentication
 	Region  string     // AWS region to operate in
-	Version = "0.0.40" // Current version of the application
+	Version = "0.1.0" // Current version of the application
 )
 
 // NewRootCmd creates and configures the root command for the AWS Simple CLI
